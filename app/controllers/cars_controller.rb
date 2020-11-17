56953c45b144d605afa51307200ce2dcdd1f57c9
Tony Karlsson <tonykarlsson79@gmail.com>
2020-11-17 12:47:54 +0100

    Create new car working

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -7,12 +7,21 @@ class CarsController < ApplicationController
   end
 
   def new
+    @car = Car.new
   end
 
   def create
+    @car = Car.new(car_params)
+    @car.user = current_user
+    if @car.save
+      redirect_to car_path(@car)
+    else
+      render :new
+    end
   end
 
   def edit
+    
   end
 
   def update
@@ -21,4 +30,9 @@ class CarsController < ApplicationController
   def destroy
   end
 
+  private
+
+  def car_params
+    params.require(:car).permit(:make, :model, :year, :seats, :transmission, :description, :price)
+  end
 end
