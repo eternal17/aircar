2be03524c56ef317161de6ac526b75460b7d9d40
Tony Karlsson <tonykarlsson79@gmail.com>
2020-11-18 11:58:10 +0100

    Booking function and total price up and running

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,8 +1,31 @@
 class BookingsController < ApplicationController
 
   def new
+    @car = Car.find(params[:car_id])
+    @booking = Booking.new
   end
 
   def create
+    @user = current_user
+    @car = Car.find(params[:car_id])
+    @booking = Booking.new(booking_params)
+    @booking.user = @user
+    @booking.car = @car
+    if @booking.save!
+      redirect_to booking_path(@booking)
+    else
+      render :new
+    end
   end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
+  end
+
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,9 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :car
+
+def total
+  
+end
+
 end
