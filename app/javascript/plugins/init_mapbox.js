1e1c808d569334792078073d206a24a4c21e2bb3
Tony Karlsson <tonykarlsson79@gmail.com>
2020-11-19 15:27:50 +0100

    Fixed map, again, hopefully

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,7 +1,6 @@
 import mapboxgl from 'mapbox-gl';
 
 const buildMap = () => {
-  mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
   return new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v10'
@@ -25,6 +24,7 @@ const fitMapToMarkers = (map, markers) => {
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
   if (mapElement) {
+    mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     const map = buildMap();
     const markers = JSON.parse(mapElement.dataset.markers);
     addMarkersToMap(map, markers);
